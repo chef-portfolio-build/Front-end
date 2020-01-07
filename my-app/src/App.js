b13df4e2e19220c68a50a5e6337081aa273032f9
Nhan <natenguyen51294@gmail.com>
2020-01-07 09:16:56 -0800

    fixed github bug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 import SignIn from './components/SignIn';
+import Register from './components/Register-Page'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <SignIn />
+          </ Route >
+
+          <Route path='/register'>
+            <Register />
+          </ Route >
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
